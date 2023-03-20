c9a62c5adddca43142841e169315d05182145353
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-03-20 23:59:33 +0530

    get routes for users

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,17 @@
 const User = require('../models/User')
 
 exports.getUsers = (req, res, next) => {
-
+    User
+        .find()
+        .then(users => {
+            res.status(200).json({message: 'Users Fetched', users: users})
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500
+            }
+            next(err)
+        }) 
 }
 
 exports.createUser = (req, res, next) => {
@@ -20,7 +30,7 @@ exports.createUser = (req, res, next) => {
             console.log(result)
             res.status(201).json({
                 message: 'User created successfully',
-                post: result
+                user: result
             })
         })
         .catch(err => {
@@ -32,5 +42,21 @@ exports.createUser = (req, res, next) => {
 }
 
 exports.getUser = (req, res, next) => {
-    
+    const userId = req.params.userId
+    Post
+        .findById(userId)
+        .then(user => {
+            if(!user) {
+                const error = new Error('Could not find user.')
+                error.statusCode = 404
+                throw error
+            }
+            res.status(200).json({ message: 'User Fetched', user: user})
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500
+            }
+            next(err)
+        })
 }
