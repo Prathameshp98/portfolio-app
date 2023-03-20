ad4f697bcbb3bf2b8fd0499293d9a9f4df7265e2
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-03-20 18:59:46 +0530

    added swagger docs

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
-const app = express();
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnptp2Db1lXb5Ysn9EAx8Wtlg2DyYnH20",
+  authDomain: "portfolio-app-1bd01.firebaseapp.com",
+  projectId: "portfolio-app-1bd01",
+  storageBucket: "portfolio-app-1bd01.appspot.com",
+  messagingSenderId: "273669198227",
+  appId: "1:273669198227:web:40aa2f6b7f7b951bb99866",
+  measurementId: "G-94N4XKY80V"
+};
+
+const customCss = fs.readFileSync((process.cwd()+"/swagger.css"), 'utf8');
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+app = express();
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {customCss}));
 
 app.listen(8080, () => {
     console.log("App started on port 8080")
