80e87b907c441b21a5eda966aea690b1a3733663
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-03-21 15:07:12 +0530

    origin

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {customCss}))
 app.use(bodyParser.json()) // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://portfolioapp-prathameshpatil2812.b4a.run')
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     next()
